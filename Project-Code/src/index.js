8c804c5e98ddb8a5d32b1d204aa3552886c0e4b4
Ryan Bas <18267769+ryanbas21@users.noreply.github.com>
2022-11-12 02:57:30 +0000

    refactor and add style guide

--- Project-Code/src/index.js
+++ Project-Code/src/index.js
@@ -1,126 +1,91 @@
-//import dependencies
-const express = require("express");
-const pgp = require("pg-promise")();
-const bodyParser = require("body-parser");
-const session = require("express-session");
+// Import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const axios = require('axios');
 const bcrypt = require('bcrypt');
 
-//define express application
+// Define express application
 const app = express();
 
-// db config
+const {API_KEY, POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USER, PORT} = process.env;
+// Db config
 const dbConfig = {
-    host: "db",
-    port: 5432,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-  };
-  
+	host: 'db',
+	port: 5432,
+	database: POSTGRES_DB,
+	user: POSTGRES_USER,
+	password: POSTGRES_PASSWORD,
+};
+
 const db = pgp(dbConfig);
 
-// db connection test
+// Db connection test
 db.connect()
-  .then((obj) => {
-    // Can check the server version here (pg-promise v10.1.0+):
-    console.log("Database connection successful");
-    obj.done(); // success, release the connection;
-  })
-  .catch((error) => {
-    console.log("ERROR:", error.message || error);
-});
+	.then(obj => {
+		// Can check the server version here (pg-promise v10.1.0+):
+		console.log('Database connection successful');
+		obj.done(); // Success, release the connection;
+	})
+	.catch(error => {
+		console.log('ERROR:', error.message || error);
+	});
 
-// set the view engine to ejs
-app.set("view engine", "ejs");
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 
-// set session
+// Set session
 app.use(
-    session({
-      secret: "XASDASDA",
-      saveUninitialized: true,
-      resave: true,
-    })
-  );
-  
-app.use(
-    bodyParser.urlencoded({
-      extended: true,
-    })
+	session({
+		secret: 'XASDASDA',
+		saveUninitialized: true,
+		resave: true,
+	}),
 );
 
-const user = {
-    username: undefined,
-    addressLine1: undefined,
-    addressLine2: undefined,
-    city: undefined,
-    state: undefined,
-    zip_code: undefined
-}
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	}),
+);
 
-const {API_KEY} = process.env
-
-app.get('/', (req, res) =>{
-    res.render('pages/login')
+app.get('/', (req, res) => {
+	res.render('pages/login');
 });
 
-//constant api call to civics API
+// Constant api call to civics API
 const civicRequest = axios.create({
-    baseURL: 'https://civicinfo.googleapis.com',
-     params: { 'apiKey': API_KEY } 
-     });
+	baseURL: 'https://civicinfo.googleapis.com',
+	params: {apiKey: API_KEY},
+});
 
-//test request for api call to polling locations with test user
-app.get('/test', async (req, res) =>{
-    //Making db query to retrieve address before API call to get voterInfo
-    const query = "SELECT addressLine1, addressLine2, city, state, zip_code FROM voters WHERE username='test';";
-    const values = [user];
+app.get('/test', async (req, res) => {
+	// Making db query to retrieve address before API call to get voterInfo
+	const query = 'SELECT addressLine1, addressLine2, city, state, zip_code FROM voters WHERE username=\'test\';';
+	const values = [{}];
 
-    //await to complete query & assign values
-    await db.any(query, values)
-    .then((data) =>{
-        user.addressLine1 = data[0].addressline1;
-        user.addressLine2 = data[0].addressline2;
-        user.city = data[0].city;
-        user.state = data[0].state;
-        user.zip_code = data[0].zip_code;
-    })
-    .catch((err) => {
-        console.log(err);
-        res.redirect("/");
-    });
+	// Await to complete query & assign values
+	try {
+		const [user] = await db.any(query, values);
 
-    //set address for api call
-    let address = `${user.addressLine1} ${user.addressLine2} ${user.city} ${user.state} ${user.zip_code}`;
-    
-    //axios API get request
-    axios({
-            url: `https://www.googleapis.com/civicinfo/v2/elections`,
-            method: 'GET',
-            //dataType:'json',
-            params: {
-                key : API_KEY,
-                address : address,
-                electionId: 2000
-            }
-        })
-         .then(results => {
-            //console.log(results.data);
-            //console.log(results.data.election);
-            console.log(results.elections.name);
-            res.render('pages/test', {
-                election: results.elections.name,
-                location: results.elections.electionDay
-            });
-         })
-        .catch((error) => {
-            //hanlde errors
-            console.log(error);
-        });
-    });
+		const address = `${user.addressLine1} ${user.addressLine2} ${user.city} ${user.state} ${user.zip_code}`;
+		const {elections} = await civicRequest('/civicinfo/v2/elections', {
+			params: {
+				address,
+				electionId: 2000,
+			},
+		});
+		res.render('pages/test', {
+			election: elections.name,
+			location: elections.electionDay,
+		});
+	} catch (error) {
+		console.log(error);
+		res.redirect('/');
+	}
+});
 
-
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+app.listen(PORT, () => {
+	console.log('listening on port 3000');
 });
