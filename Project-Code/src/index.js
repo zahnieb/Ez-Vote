894a2afee6d8b486f460c934960f199dec36cf26
Tome Dudanov <todu7268@colorado.edu>
2022-11-14 14:28:18 -0700

    minor fixes made

--- Project-Code/src/index.js
+++ Project-Code/src/index.js
@@ -5,11 +5,10 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const axios = require('axios');
 const bcrypt = require('bcrypt');
-
+const { nextTick } = require("process");
 
 //define express application
 const app = express();
-
 
 // db config
 const dbConfig = {
@@ -63,84 +62,183 @@ const user = {
     zip_code: undefined,
 };
 
+const {API_KEY} = process.env
 
-app.get('/', (req, res) => {
-    res.render('pages/login')
+
+app.get('/', (req, res) =>{
+    res.redirect('/wciv') //if user was logged in and closed the site, when he opens it he'll be redirected to wciv.
+    //if he's not logged in, wciv will just redirect him to the login page anyway.
 });
 
-app.get('/test', (req, res) => {
+//test request for api call to polling locations with test user
+app.get('/test', async (req, res) =>{
+    //Making db query to retrieve address before API call to get voterInfo
+    const query = `SELECT addressLine1, addressLine2, city, state, zip_code FROM voters WHERE username='test';`;
+    const values = [user];
+
+    //await to complete query & assign values
+    await db.any(query, values)
+    .then((data) =>{
+        user.addressLine1 = data[0].addressline1;
+        user.addressLine2 = data[0].addressline2;
+        user.city = data[0].city;
+        user.state = data[0].state;
+        user.zip_code = data[0].zip_code;
+    })
+    .catch((err) => {
+        console.log(err);
+        res.redirect("/");
+    });
+
+    //set address for api call
+    let address = `${user.addressLine1} ${user.addressLine2} ${user.city} ${user.state} ${user.zip_code}`;
+    
+    //axios API get request
     axios({
-         url: `https://www.googleapis.com/civicinfo/v2/voterinfo?address=24%20Scott%20Drive%20broomfield%20CO&includeOffices=true&levels=regional&roles=governmentOfficer&key=${process.env.API_KEY}`,
+            url: `https://www.googleapis.com/civicinfo/v2/elections`,
             method: 'GET',
-            dataType:'json',
-            param: {
-                "address": "24 Scott Drive broomfield co",
-                "includeOffices": true,
-                "levels":[
-                    "regional"
-                ],
-                "roles": [
-                    "governmentOfficer"
-                ]
+            //dataType:'json',
+            params: {
+                key : API_KEY,
+                //address : address,
+                electionId: 2000
             }
-         })
+        })
          .then(results => {
-            //console.log(results.data);
-            //console.log(results.data.election);
-            console.log(results.data.pollingLocations);
+            console.log(results);
+            console.log(results.data.elections[0].name);
+            console.log(results.data.elections[0].electionDay);
             res.render('pages/test', {
-                election: results.data.election,
-                location: results.data.pollingLocations
+                election: results.data.elections[0],
+                date: results.data.elections[0]
             });
-        })
-        .catch(error => {
+
+         })
+        .catch((error) => {
             //hanlde errors
-            res.render({
-                results: [],
-                error: error,
-            })
-        })
-});
+            console.log(error);
+        });
+    });
 
-app.get('/login', (req, res) =>{
-    res.render('pages/login')
-});
+app.get('/info', (req, res) => {
 
-app.get('/info', (req, res) =>{
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+
     res.render('pages/info')
 });
 
-app.get('/register', (req, res) =>{
-    res.render('pages/register')
-});
+app.get('/wciv', async (req, res) =>{
+    if(!req.session.user){
+        return res.redirect('/login');
+    }
 
-app.get('/wciv', (req, res) =>{
-    res.render('pages/wciv')
+     //Making db query to retrieve address before API call to get voterInfo
+     const query = `SELECT addressLine1, addressLine2, city, state, zip_code FROM voters WHERE username='test';`;
+     const values = [user];
+ 
+     //await to complete query & assign values
+     await db.any(query, values)
+     .then((data) =>{
+         user.addressLine1 = data[0].addressline1;
+         user.addressLine2 = data[0].addressline2;
+         user.city = data[0].city;
+         user.state = data[0].state;
+         user.zip_code = data[0].zip_code;
+     })
+     .catch((err) => {
+         console.log(err);
+         res.redirect("/login");
+     });
+ 
+     //set address for api call
+     let address = `${user.addressLine1} ${user.addressLine2} ${user.city} ${user.state} ${user.zip_code}`;
+     
+     //axios API get request
+     axios({
+             url: `https://www.googleapis.com/civicinfo/v2/elections`,
+             method: 'GET',
+             //dataType:'json',
+             params: {
+                 key : API_KEY,
+                 //address commented until we figure out what to do about API error as elections are over and returns are invalid
+                 //address : address,
+                 electionId: 2000
+             }
+         })
+          .then(results => {
+             console.log(results);
+             console.log(results.data.elections[0].name);
+             console.log(results.data.elections[0].electionDay);
+             res.render('pages/wciv', {
+                 election: results.data.elections[0],
+                 date: results.data.elections[0],
+                 results: results
+             });
+ 
+          })
+         .catch((error) => {
+             //hanlde errors
+             console.log(error);
+         });
 });
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
 });
 
-app.get('/register', (req, res) => {
-    res.render('pages/register', {});
+app.get('/login', (req, res) => {
+    if(req.session.user){
+        return res.redirect('/wciv');
+    }
+    res.render('pages/login');
 });
 
-app.get('/login', (req, res) => {
-    console.log(user.username + 
-        user.password + 
-        user.addressLine1 + 
-        user.addressLine2 +
-        user.city + 
-        user.state + 
-        user.zip_code);
-    res.render('pages/login', {});
+//user Sign in
+app.post('/login', async (req, res) => {
+    const user = req.body.username;
+    const query = "SELECT * FROM voters WHERE username = $1";
+    const values = [user];
+    
+
+    await db.one(query,values)
+        .then(async (data)=> {
+            user.username = data.username;
+            user.password = data.password;
+            const match = await bcrypt.compare(req.body.password, data.password);
+
+            console.log(match);
+            if (match != false){
+                //add error message in message.ejs call
+                //test case for false ** return res.json({success: false, message: 'passwords do not match'}); **
+                res.render('pages/login', {message:"Incorrect username or password."}) //if passwords don't match, render with a message.
+            } else {
+                req.session.user = {
+                    username: user.username
+                }
+                console.log(req.session.user);
+                req.session.save();
+            }
+            
+            res.redirect("/wciv");
+            })
+        .catch((err) =>{
+            console.log(err);
+            res.render("pages/login", {message:"Incorrect username or password."}) //if username doesn't exist in database, render with message.
+        });
+});
+
+
+app.get('/register', (req, res) => {
+    res.render('pages/register');
 });
 
 // Register submission
 app.post('/register', async (req, res) => {
     const query = 'INSERT INTO voters (username, password, addressLine1, addressLine2, city, state, zip_code) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-    //const hash = await bcrypt.hash(req.body.password, 10);
+    //hash password for register
+    const hash = await bcrypt.hash(req.body.password, 10);
     db.any(query, [
         req.body.username,
         req.body.password,
@@ -152,7 +250,7 @@ app.post('/register', async (req, res) => {
     ])
         .then(function (data) {
             user.username = req.body.username;
-            user.password = req.body.password;
+            hash.password = req.body.password;
             user.addressLine1 = req.body.addressLine1;
             user.addressLine2 = req.body.addressLine2;
             user.city = req.body.city;
@@ -162,6 +260,12 @@ app.post('/register', async (req, res) => {
         })
         .catch(function (err) {
             console.log(err);
-            res.redirect('/register');
+            res.render('pages/register', { message: "That username is already taken." });
+            //added error message functionality if username entered is taken.
         });
 });
+
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect("/login");
+  });
