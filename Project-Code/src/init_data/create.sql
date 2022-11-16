e13c1103147f668bef7798c759ca96805bc1c436
Jason Swartz <jasw8976@colorado.edu>
2022-11-15 19:55:58 -0700

    fixing login post, updates to bcrypt stuff, changed lots of user.vars to req.session.user.vars

--- Project-Code/src/init_data/create.sql
+++ Project-Code/src/init_data/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE voters(
     username VARCHAR(20) PRIMARY KEY,
-    password VARCHAR(24) NOT NULL,
+    password CHAR(60) NOT NULL,
     addressLine1 VARCHAR(40) NOT NULL,
     addressLine2 VARCHAR(40),
     city VARCHAR(30) NOT NULL,
